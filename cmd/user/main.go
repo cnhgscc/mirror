a3409173c036fefd3532bc0e3e98bd5dc71c9731
Austin <austin@example.com>
2023-08-08 01:00:56 +0800

    fix

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,49 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	dbp "mirror/pkg/dbplugin"
-	"mirror/pkg/gomysql"
-)
-
-type Product struct {
-	dbp.M
-	Code  string
-	Price uint
-}
-
 func main() {
-	err := gomysql.Init(&gomysql.Config{
-		Addr: "127.0.0.1",
-		User: "root",
-		Pass: "123456",
-		DB:   "lucky",
-	})
 
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	db := gomysql.S("lucky")
-
-	// Migrate the schema
-	db.AutoMigrate(&Product{})
-
-	// Create
-	db.Create(&Product{Code: "D42", Price: 100})
-
-	// Read
-	var product Product
-	db.First(&product, 1)                 // find product with integer primary key
-	db.First(&product, "code = ?", "D42") // find product with code D42
-
-	// Update - update product's price to 200
-	db.Model(&product).Update("Price", 200)
-	// Update - update multiple fields
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-
-	// Delete - delete product
-	db.Delete(&product, 1)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module mirror
 go 1.19
 
 require (
+	google.golang.org/grpc v1.57.0
+	google.golang.org/protobuf v1.31.0
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.2
 	gorm.io/plugin/soft_delete v1.2.1
@@ -10,6 +12,11 @@ require (
 
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
 )
