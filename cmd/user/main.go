8cb4469cdd5c5d6eca5bc8c7aec32ae9d20d132a
Austin <austin@example.com>
2023-08-08 01:20:06 +0800

    fix

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"net"
+
+	"google.golang.org/grpc"
+
+	"mirror/pkg/pb"
+
+	"mirror/internal/pkg/usecase"
+)
+
 func main() {
+
+	lis, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		return
+	}
+
+	g := grpc.NewServer()
+	pb.RegisterGreeterServer(g, new(usecase.GrpcGreater))
+	_ = g.Serve(lis)
 
 }
---
+++ internal/pkg/usecase/greater.go
@@ -0,0 +1,23 @@
+package usecase
+
+import (
+	"context"
+
+	"mirror/pkg/pb"
+)
+
+var (
+	_ pb.GreeterServer = (*GrpcGreater)(nil)
+)
+
+type GrpcGreater struct {
+	pb.GreeterServer
+}
+
+func (g GrpcGreater) SayHello(ctx context.Context, request *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: request.Name + ":1"}, nil
+}
+
+func (g GrpcGreater) SayHelloAgain(ctx context.Context, request *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: request.Name + "2"}, nil
+}
