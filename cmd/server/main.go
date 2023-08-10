e40208cc4fb21bdf2287f1b5acfa39ac6c250ae2
Austin <austin@example.com>
2023-08-11 04:01:41 +0800

    fix

---
+++ cmd/server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+
+	"mirror/pkg/build"
+	"mirror/pkg/cmdargs"
+	"mirror/pkg/cregistry"
+	"mirror/pkg/pb"
+
+	"mirror/internal/pkg/usecase"
+)
+
+func init() {
+	cmdargs.Init()
+}
+
+func main() {
+
+	cr, err := cregistry.NewCRegistry(build.CMDName(), "127.0.0.1:9000")
+	if err != nil {
+		return
+	}
+	cr.Register()
+	defer cr.UNRegister()
+
+	addr := fmt.Sprintf("%v:%v", viper.Get("server.host"), viper.Get("server.port"))
+	fmt.Println("serve: " + addr)
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		return
+	}
+	g := grpc.NewServer()
+	pb.RegisterGreeterServer(g, new(usecase.GrpcGreater))
+	_ = g.Serve(lis)
+
+}
