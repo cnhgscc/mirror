e71c7670ac54c580f618638686a44d7c8081d4fd
austin <18511828634@163.com>
2023-08-17 14:14:23 +0800

    fix

--- cmd/spider/main.go
+++ cmd/spider/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/cnhgscc/mirror/pkg/httpd"
 )
 
 func main() {
 
-	resp := map[string]any{}
-	reply, err := httpd.NewURL(httpd.NewRequest("https://www.baidu.com")).JSONRespRender(&resp)
-	if err != nil {
-		return
-	}
-	fmt.Println(reply)
+	reply := map[string]any{}
+
+	req := httpd.NewRequest("http://127.0.0.1:8003/api/v1/platform/games", httpd.WithMethod("POST"), httpd.WithJSONBody("3123123"))
+
+	resp := httpd.NewURL(req, func(header *http.Header) {
+		header.Set("TX-Client-Version", "1.0.0")
+	})
+
+	_, _ = resp.JSONRespRender(&reply)
+
+	fmt.Println(resp.Status)
+	fmt.Println(resp, reply)
 
 }
--- pkg/httpd/open.go
+++ pkg/httpd/open.go
@@ -1,5 +1,7 @@
 package httpd
 
+import "net/http"
+
 var (
 	client *Client
 )
@@ -8,7 +10,12 @@ func init() {
 	client = NewClient()
 }
 
-func NewURL(req *Request) *Resp {
+func NewURL(req *Request, header ...Header) *Resp {
 	// req.Request.Method = strings.ToUpper(method)
+	for _, h := range header {
+		h(&req.Header)
+	}
 	return client.NewURL(req)
 }
+
+type Header func(header *http.Header)
